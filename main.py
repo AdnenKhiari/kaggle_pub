60437cf7b1105de9e7922e5ba2491f767094d07a
AdnenKhiari <aymenkhiari484@gmail.com>
2025-02-19 15:40:28 +0100

    fix

--- main.py
+++ main.py
@@ -239,6 +239,7 @@ def train(model, epoch,mini_batch_size,total_batch_size, train_loader,val_loader
 
             # Gradient Accumulation
             if (batch_idx) % grad_acc_steps == 0:
+                print("GRAD UPDATE")
                 scaler.unscale_(optim)
                 torch.nn.utils.clip_grad_norm_(model.parameters(),1.0)
                 scaler.step(optim)
